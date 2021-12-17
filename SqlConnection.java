ca0bc257b2bee1b23fef5602b08fc22e19db2377
isaccmatei <88686368+isaccmatei@users.noreply.github.com>
2021-12-17 11:53:21 +0200

    Add files via upload

---
+++ SqlConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+
+/**
+ *
+ * @author Mate
+ */
+public class SqlConnection {
+public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+              Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/gms","root","admin123");
+            return con;
+        } 
+        catch(Exception e)
+        {
+            return null;
+        }
+    }   
+    
+}
